c0b4c26eb2576aa99dd8836ae7b207fcb528c12b
Helazhary <Helazhary@aucegypt.edu>
2023-11-03 17:55:28 +0200

    Added basic login screen and button test

--- app/src/main/java/com/scrumptious/algorithmvisualizer/MainActivity.kt
+++ app/src/main/java/com/scrumptious/algorithmvisualizer/MainActivity.kt
@@ -2,10 +2,21 @@ package com.scrumptious.algorithmvisualizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the login button by its ID
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        // Set an onClickListener for the login button
+        loginButton.setOnClickListener {
+            // Show a toast message when the login button is clicked
+            Toast.makeText(this, "Login clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
