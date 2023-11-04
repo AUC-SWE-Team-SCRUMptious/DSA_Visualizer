c813db669f2414d1e83977890720009ccdfe2ec4
Ahmed-A <ahmed-a@aucegypt.edu>
2023-11-05 01:22:14 +0200

    Update BubbleSortSequenceGenerator.kt
    Added test cases as a comment

--- app/src/main/java/com/scrumptious/algorithmvisualizer/BubbleSortSequenceGenerator.kt
+++ app/src/main/java/com/scrumptious/algorithmvisualizer/BubbleSortSequenceGenerator.kt
@@ -25,6 +25,7 @@ fun bubbleSortSequenceGenerator(inputArr: Array<Int>): Array<Pair<Array<Int>, Ar
 
     return stepsAndSwaps.toTypedArray()
 }
+/*
 
 fun displaySteps(inputArr: Array<Int>, steps: Array<Pair<Array<Int>, Array<Int>>>) {
     println("Original Array: ${inputArr.joinToString()}")
@@ -50,3 +51,40 @@ fun main() {
         println()
     }
 }
+
+Test Cases:
+
+Original Array: 5, 6, 7, 1, 1
+Step 1: 5, 6, 1, 7, 1, Indices Swapped: 2, 3
+Step 2: 5, 6, 1, 1, 7, Indices Swapped: 3, 4
+Step 3: 5, 1, 6, 1, 7, Indices Swapped: 1, 2
+Step 4: 5, 1, 1, 6, 7, Indices Swapped: 2, 3
+Step 5: 1, 5, 1, 6, 7, Indices Swapped: 0, 1
+Step 6: 1, 1, 5, 6, 7, Indices Swapped: 1, 2
+
+Original Array: 43, 32, 23, 54, 55
+Step 1: 32, 43, 23, 54, 55, Indices Swapped: 0, 1
+Step 2: 32, 23, 43, 54, 55, Indices Swapped: 1, 2
+Step 3: 23, 32, 43, 54, 55, Indices Swapped: 0, 1
+
+Original Array: 5, 7, 10, 8, 3
+Step 1: 5, 7, 8, 10, 3, Indices Swapped: 2, 3
+Step 2: 5, 7, 8, 3, 10, Indices Swapped: 3, 4
+Step 3: 5, 7, 3, 8, 10, Indices Swapped: 2, 3
+Step 4: 5, 3, 7, 8, 10, Indices Swapped: 1, 2
+Step 5: 3, 5, 7, 8, 10, Indices Swapped: 0, 1
+
+Original Array: 1, 2, 3, 4, 5
+
+Original Array: 103, 35, 34, 23, 11
+Step 1: 35, 103, 34, 23, 11, Indices Swapped: 0, 1
+Step 2: 35, 34, 103, 23, 11, Indices Swapped: 1, 2
+Step 3: 35, 34, 23, 103, 11, Indices Swapped: 2, 3
+Step 4: 35, 34, 23, 11, 103, Indices Swapped: 3, 4
+Step 5: 34, 35, 23, 11, 103, Indices Swapped: 0, 1
+Step 6: 34, 23, 35, 11, 103, Indices Swapped: 1, 2
+Step 7: 34, 23, 11, 35, 103, Indices Swapped: 2, 3
+Step 8: 23, 34, 11, 35, 103, Indices Swapped: 0, 1
+Step 9: 23, 11, 34, 35, 103, Indices Swapped: 1, 2
+Step 10: 11, 23, 34, 35, 103, Indices Swapped: 0, 1
+*/
