b41646449b475540f9ae8f0849eeaf19399fe48e
OmarEflouly <98827931+OmarElfouly@users.noreply.github.com>
2023-11-13 12:02:22 +0200

    Update DataValidator.kt
    Adds number of elements testing and refers to a null input as a nonnumber

--- app/src/main/java/com/scrumptious/algorithmvisualizer/DataValidator.kt
+++ app/src/main/java/com/scrumptious/algorithmvisualizer/DataValidator.kt
@@ -2,19 +2,19 @@ package com.scrumptious.algorithmvisualizer
 
 //import android.provider.ContactsContract.Data
 import java.lang.NumberFormatException
-
+// Does not accept " 1,2,,3"
 class DataValidator {
-    var dataStatus: String = ""
-    var validData = mutableListOf<Int>()
+    var DataStatus: String = ""
+    var ValidData = mutableListOf<Int>()
     fun validate( userInput: String){
         val myInts = userInput.split(",")
-        val currentData = mutableListOf<Int>()
+        var currentData = mutableListOf<Int>()
         var sorted = true
-        for(i in myInts.indices){
+        for(i in 0 until myInts.size){
             try {
                 val num = myInts[i].trim().toLong() // will fail if input is not an integer
                 if(num < -2147483648 || num > 2147483647){
-                    dataStatus = "Input is out of range"
+                    DataStatus = "Input is out of range"
                     return
                 }
                 if(i != 0 && num.toInt() < currentData[i-1]){
@@ -23,15 +23,19 @@ class DataValidator {
                 currentData.add(num.toInt())
             }
             catch (e: NumberFormatException){
-                dataStatus = "Input includes a non number"
+                DataStatus = "Input includes a non number"
                 return
             }
         }
+        if(currentData.size >20){
+            DataStatus = "Input has more than 20 elements"
+            return
+        }
         val message = if (sorted) "sorted" else "not sorted" // Only sorted if ascending
-        dataStatus = "Input is valid and $message"
-        validData = currentData
+        DataStatus = "Input is valid and $message"
+        ValidData = currentData
 
-    }
+	}
 }
 /*
 Test Cases:
@@ -48,4 +52,45 @@ Input                           Expected Output                     Output
 "1,a,2"                         Input includes a non number         Input includes a non number
 "1,a2,3"                        Input includes a non number         Input includes a non number
 "1,,2,3"                        Input includes a non number         Input includes a non number
+""                              Input includes a non number         Input includes a non number
+"apples"                        Input includes a non number         Input includes a non number
+"1,2,3,4,5,6,7,8,9,10,11,
+12,13,14,15,16,17,18,19,20"     Input is valid and sorted           Input is valid and sorted
+"1,2,3,4,5,6,7,8,9,10,11,12,
+13,14,15,16,17,18,19,20,21"     Input has more than 20 elements     Input has more than 20 elements
  */
+
+
+/* Driver code for testing
+fun main(){
+
+    val inputArrays = arrayOf(
+        "-99, 99, 2147483648",
+        "-2147483648, 99, 2147483647",
+        "2147483647, 99, -2147483648",
+       	"1,2,3" ,
+        "3,2,1",
+        "1",
+        "a",
+        "a,1,2",
+        "1,a,2",
+        "1,a2,3",
+        "1,,2,3",
+        "",
+        "apples",
+        "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20",
+        "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21"
+    )
+
+    val i = DataValidator()
+    for (str in inputArrays) {
+        println("Input is: ")
+        println(str)
+        i.validate(str)
+        println("Result:")
+        println(i.DataStatus)
+        println(i.ValidData)
+        println()
+    }
+}
+*/
